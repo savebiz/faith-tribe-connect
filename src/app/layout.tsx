3458d863922c838c7a4a4ae9cfe61dbc85323136
savebiz <victor.sabo@example.com>
2025-12-26 23:31:46 +0100

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Faith Tribe Digital Platform",
+  description: "The Digital Tabernacle for RCCG Region 63 Teens",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scoll-smooth">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background text-foreground`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
